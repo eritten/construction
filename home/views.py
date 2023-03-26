e38b78fdf290d8cc2223a036bbe7ac97d9562d1b
= <=>
2023-03-26 09:43:14 -0200

    add home template and static files

--- home/views.py
+++ home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def terms(request):
+    return render(request, "terms.html")
+
+def privacy(request):
+    return render(request, "privacy.html")
