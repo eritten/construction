9730d19b06a47b615fc4339f20b2a71933a4c633
= <=>
2023-03-26 18:05:54 -0200

    add changes

--- construction/urls.py
+++ construction/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from home import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 path('', views.home, name='home'),
@@ -10,3 +12,6 @@ path('terms/', views.terms, name='terms'),
 path('invoice/', views.invoice, name='invoice'),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=MEDIA_ROOT)
