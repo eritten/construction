1ef51f35e13c6b9badf90f841f178fa1c9890d12
Klasique <feboapong@gmail.com>
2023-09-28 13:39:04 +0100

    yes

--- construction/settings.py
+++ construction/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1q(zk-2n*!xm^lmc7%o9lhsz(iml^0h17b1_52^#-yv07nfbk+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
